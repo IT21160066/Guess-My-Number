5a3f26e40343a6733c316583d0ef29d40a0ac0d4
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-04 16:54:52 +0530

    changes

--- src/script.js
+++ src/script.js
@@ -16,16 +16,20 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '😯 No number!';
+    displayMessage('😯 No number!');
 
     //when player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '😎 Correct number!';
+    displayMessage('😎 Correct number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').style.borderRadius = '50rem';
@@ -37,25 +41,13 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     //when guess is too high
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '🙄 Too high!';
+      displayMessage(guess > secretNumber ? '🙄 Too high!' : '🙄 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You loss the game';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = 'red';
-    }
-
-    //when guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '🙄 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You loss the game';
+      displayMessage('You loss the game');
       document.querySelector('.score').textContent = 0;
       document.querySelector('body').style.backgroundColor = 'red';
     }
@@ -66,7 +58,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
